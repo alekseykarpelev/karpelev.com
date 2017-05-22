7d41e2f0fa1a04fac49d4d0bb76029e39450d111
Aleksey <alekseykarpelev@gmail.com>
2017-05-22 12:31:27 +1000

    some work

--- index.php
+++ index.php
@@ -67,16 +67,11 @@ $DB = new Db(DBHost, DBName, DBUser, DBPassword);
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Пользователь <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
+                <li><a href="#">Регистрация</a></li>  
+                 <li><a href="#">Вход</a></li>  
+          </ul>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
@@ -99,9 +94,38 @@ $DB = new Db(DBHost, DBName, DBUser, DBPassword);
         <p>
           <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
         </p>
-      </div>
+        
+          <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
 
     </div> <!-- /container -->
+        
+        
+        
+        
+        
+      </div>
+
+    </div>
+    
+  
+    
+    
+    
+     <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
